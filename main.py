33eae00c572a86b56b13cceb5dba4a155336e179
Anton <antonsushilov@mail.ru>
2024-01-12 17:22:47 +0300

    refactor logic

--- main.py
+++ main.py
@@ -27,13 +27,41 @@ def pdf_to_image(input_dir, file, img_dir, main_dir):
     image_to_text(img_dir, res_dir, file)
 
 
-def image_to_text(img_dir, output_dir, file):
+def text_processing(text):
+    text = text.split(",")
+    text = text[1:]
+    for i, t in enumerate(text):
+        t = t.strip().split(" ")
+        print(t)
+        if len(t) > 1:
+            if t[1] == "р-н" or t[1] == "обл" or t[0] == "дом" or t[0] == "корпус":
+                continue
+            else:
+                t.insert(0, t.pop())
+                t[0] = t[0] + "."
+                text[i] = " ".join(t)
+    text = ", ".join(text)
+    print(text)
+    return text
+
+
+def save_to_txt(file, file_img, output_dir, text):
     name = file.split(".")[0] + '.txt'
+    # name_page = file_img.split(".")[0]
+    print(file_img)
+    text = file_img + "+" + text.strip().replace("  ", " ")
+    path = os.path.join(output_dir, name)
+    with open(path, "a") as f:
+        f.writelines(text + "\n")
+
+
+
+def image_to_text(img_dir, output_dir, file):
     files = len(os.listdir(img_dir))
     dir_name = os.path.split(img_dir)[-1]
     files = [dir_name + "_page " + str(i + 1) + ".jpg" for i in range(files)]
-    for file in files:
-        image = os.path.join(img_dir, file)
+    for file_img in files:
+        image = os.path.join(img_dir, file_img)
         img = cv2.imread(image)
         # percent by which the image is resized
         scale_percent = 50
@@ -45,30 +73,14 @@ def image_to_text(img_dir, output_dir, file):
         # resize image
         small_img = cv2.resize(img, dsize)
         crop_img = small_img[300:400, 220:1400]
-        cv2.imshow("address", crop_img)
-        cv2.waitKey(0)
+        # cv2.imshow("address", crop_img)
+        # cv2.waitKey(0)
         img = cv2.cvtColor(crop_img, cv2.COLOR_BGR2RGB)
         config = r'--oem 3 --psm 6 -c page_separator='''
         text = pytesseract.image_to_string(img, lang='rus', config=config).replace("\n", " ").strip()
-        text = text.split(",")
-        text = text[1:]
-        for i, t in enumerate(text):
-            t = t.strip().split(" ")
-            print(t)
-            if len(t) > 1:
-                if t[1] == "р-н" or t[1] == "обл" or t[0] == "дом" or t[0] == "корпус":
-                    continue
-                else:
-                    t.insert(0, t.pop())
-                    t[0] = t[0] + "."
-                    text[i] = " ".join(t)
-        text = ", ".join(text)
-        text = file + "+" + text.strip().replace("  ", " ")
-        print(text)
-        path = os.path.join(output_dir, name)
-        f = open(path, 'a')
-        f.writelines(text + "\n")
-        f.close()
+        text = text_processing(text)
+        save_to_txt(file, file_img, output_dir, text)
+        
 
 
 def main():
